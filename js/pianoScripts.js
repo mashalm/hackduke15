097509e59e213f2587db5300cd792863d25c9e2c
Joon Kyung Kim <jkim796@gatech.edu>
2015-11-08 10:39:10 -0500

    commit

--- js/pianoScripts.js
+++ js/pianoScripts.js
@@ -2,6 +2,7 @@
 $( document ).ready(function() {
     mapPianoKeys();
     $('pianoKeyMap').imageMapResize();
+    captureGestures();
 });
 
 function mapPianoKeys() {
@@ -75,4 +76,28 @@ function setOpacity(id, level) {
 
 function showId(id) {
 	console.log(id);
+}
+
+function captureGestures() {
+    var controller = Leap.loop({enableGestures: true}, function(frame) {
+	var gestures = frame.gestures;
+	var hands = frame.hands;
+	if (rightHand = frame.hands[0])
+	    console.log(rightHand.screenPosition());
+	if (frame.valid && gestures.length > 0) {
+	    gestures.forEach(function(gesture) {
+		var handIds = gesture.handIds;
+		handIds.forEach(function(handId) {
+		    var hand = frame.hand(handId);
+		    $('#positionSpan').html(hand.screenPosition().join(','));
+		});
+	    });
+	}
+    }).use('screenPosition', {scale: 0.25});
+
+    controller.use('boneHand', {
+	targetEl: document.body,
+	arm: true,
+	opacity: 0.5,
+    });
 }
